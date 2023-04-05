c567f8d3dccccaf7a48ceadede54b84368c5864c
Nguyễn Đức Phú <104358167+KizEvo@users.noreply.github.com>
2023-04-05 23:07:49 +0700

    Updated moves and rotate functions
    Changed from TinkerCAD simulation to IRL Arduino code

--- index.ino
+++ index.ino
@@ -1,172 +1,168 @@
-#define MOTOR_DRIVE_1 3
-#define MOTOR_DIRECTION_1 2
+#define IR_SENSOR_RIGHT 11
+#define IR_SENSOR_LEFT 12
+#define MOTOR_SPEED 180
 
-#define MOTOR_DRIVE_2 5
-#define MOTOR_DIRECTION_2 4
+//Right motor
+int enableRightMotor = 6;
+int rightMotorPin1 = 9;
+int rightMotorPin2 = 10;
 
-#define MOTOR_DRIVE_3 6
-#define MOTOR_DIRECTION_3 7
+//Left motor
+int enableLeftMotor = 5;
+int leftMotorPin1 = 7;
+int leftMotorPin2 = 8;
 
-#define MOTOR_DRIVE_4 9
-#define MOTOR_DIRECTION_4 8
+//Sensor3
+int Backup_sensor = 4;
 
-#define ULTSON_SENSOR_RIGHT 12
-#define ULTSON_SENSOR_LEFT 13
+//Ultra sonic
+int TRIG = 2;
+int ECHO = 3;
 
-#define MAX_FORWARD_SPEED 250
-#define MAX_REVERSE_SPEED 10
-
-// Các CONSTANT cho các biến khác nhau, 5 và 0 ở đây là HIGH và LOW
-// MOVE_STOP = 1 chỉ để lặp điều kiện
-const int ROTATE_LEFT = 5;
-const int ROTATE_RIGHT = 0;
-
-const int MOVE_REVERSE = 5;
-const int MOVE_FORWARD = 0;
-const int MOVE_STOP = 1;
-
-
-// SETUP các chân trên Arduino
 void setup()
 {
-  pinMode(MOTOR_DRIVE_1, OUTPUT);
-  pinMode(MOTOR_DIRECTION_1, OUTPUT);
+  //set frequecny as 7812.5 hz.
+  TCCR0B = TCCR0B & B11111000 | B00000010 ;
   
-  pinMode(MOTOR_DRIVE_2, OUTPUT);
-  pinMode(MOTOR_DIRECTION_2, OUTPUT);
+  // put your setup code here, to run once:
+  pinMode(enableRightMotor, OUTPUT);
+  pinMode(rightMotorPin1, OUTPUT);
+  pinMode(rightMotorPin2, OUTPUT);
   
-  pinMode(MOTOR_DRIVE_3, OUTPUT);
-  pinMode(MOTOR_DIRECTION_3, OUTPUT);
-  
-  pinMode(MOTOR_DRIVE_4, OUTPUT);
-  pinMode(MOTOR_DIRECTION_4, OUTPUT);
-   
-  Serial.begin(9600);
+  pinMode(enableLeftMotor, OUTPUT);
+  pinMode(leftMotorPin1, OUTPUT);
+  pinMode(leftMotorPin2, OUTPUT);
+
+  pinMode(IR_SENSOR_RIGHT, INPUT);
+  pinMode(IR_SENSOR_LEFT, INPUT);
+
+  rotateMotor(0,0);   
+  pinMode(Backup_sensor,INPUT);
 }
 
-// Hàm loop chính của chương trình, các hàm phụ sẽ được gọi ở đây.
-// Không nên gọi các hàm config hoặc các hàm con của hàm phụ ở đây.
+
 void loop()
 {
-  moveReverse();
-}
-
-// Hàm điều chỉnh hướng di chuyển của bánh xe 
-// hàm config
-void motorDirectionConfig(int mode){
-  if(mode == MOVE_STOP){
-    digitalWrite(MOTOR_DIRECTION_1, MOVE_FORWARD);
-    digitalWrite(MOTOR_DIRECTION_2, MOVE_FORWARD);
-    digitalWrite(MOTOR_DIRECTION_3, MOVE_FORWARD);
-    digitalWrite(MOTOR_DIRECTION_4, MOVE_FORWARD);
-    return;
+  delay(50);
+  int distance = sensorDetectionRange(TRIG, ECHO);     // doc khoang cach
+  if (distance <= 15 && distance > 0)
+  {
+    rotateMotor(0,0);
+    delay(50);
+    rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);
+    delay(100);
+    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
+    delay(300);
+    rotateMotor(-MOTOR_SPEED, MOTOR_SPEED);
+    delay(100);
   }
-  if(mode == MOVE_FORWARD) {
-    digitalWrite(MOTOR_DIRECTION_1, MOVE_FORWARD);
-    digitalWrite(MOTOR_DIRECTION_2, MOVE_FORWARD);
-    digitalWrite(MOTOR_DIRECTION_3, MOVE_FORWARD);
-    digitalWrite(MOTOR_DIRECTION_4, MOVE_FORWARD);
-    return;
+
+  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
+  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+
+  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
+  {
+    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
   }
-  // PORT ở mức thấp (0V/LOW) thì bánh xe sẽ được thiết lập tiến về trước
-
-  digitalWrite(MOTOR_DIRECTION_1, MOVE_REVERSE);
-  digitalWrite(MOTOR_DIRECTION_2, MOVE_REVERSE);
-  digitalWrite(MOTOR_DIRECTION_3, MOVE_REVERSE);
-  digitalWrite(MOTOR_DIRECTION_4, MOVE_REVERSE);
-  // PORT ở mức cao (5V/HIGH) thì bánh xe sẽ được tiếp lập tiến về phía sau
-}
-
-// Hàm điều chỉnh hướng quay của bánh xe 
-// hàm config
-void motorRotateConfig(int mode){
-  int motor13Direction = mode == ROTATE_RIGHT ? 5 : 0;
-  int motor24Direction = mode == ROTATE_LEFT ? 5 : 0;
-  // nếu như so sánh ở motor13 là TRUE và motor24 là FALSE thì chế độ bánh xe đang là hướng quay phải (RIGHT)
-  digitalWrite(MOTOR_DIRECTION_1, motor13Direction);
-  digitalWrite(MOTOR_DIRECTION_3, motor13Direction);
-  
-  digitalWrite(MOTOR_DIRECTION_2, motor24Direction);
-  digitalWrite(MOTOR_DIRECTION_4, motor24Direction);
-}
-
-// Hàm dừng bánh xe, nhận 1 param là khoảng thời gian chờ 
-// hàm phụ
-// moveStop(timeInMs), 
-// EXAMPLE: moveStop(2000);
-// timeInMs = 2000 milisec
-void moveStop(unsigned long motorWaitTimeInMs){
-  motorDirectionConfig(MOVE_STOP);
-  delay(100);
-  
-  analogWrite(MOTOR_DRIVE_1,0);
-  analogWrite(MOTOR_DRIVE_2,0);
-  analogWrite(MOTOR_DRIVE_3,0);
-  analogWrite(MOTOR_DRIVE_4,0);
-  
-  delay(motorWaitTimeInMs);
-}
-
-// Hàm di chuyển về phía trước 
-// hàm phụ
-void moveForward(){  
-  motorDirectionConfig(MOVE_FORWARD);
-  delay(100);
-  
-  digitalWrite(MOTOR_DRIVE_1, MAX_FORWARD_SPEED);
-  digitalWrite(MOTOR_DRIVE_2, MAX_FORWARD_SPEED);
-  digitalWrite(MOTOR_DRIVE_3, MAX_FORWARD_SPEED);
-  digitalWrite(MOTOR_DRIVE_4, MAX_FORWARD_SPEED);
-}
-
-// Hàm di chuyển về phía sau 
-// hàm phụ
-void moveReverse(){
-  motorDirectionConfig(MOVE_REVERSE);
-  delay(100);
-  
-  analogWrite(MOTOR_DRIVE_1, MAX_REVERSE_SPEED);
-  analogWrite(MOTOR_DRIVE_2, MAX_REVERSE_SPEED);
-  analogWrite(MOTOR_DRIVE_3, MAX_REVERSE_SPEED);
-  analogWrite(MOTOR_DRIVE_4, MAX_REVERSE_SPEED);
-}
-
-// Hàm thực thi việc quay bánh xe qua phải 
-// hàm phụ
-void motorRotateRight(){
-  int timeToRotate;
-  int speedForward = 100;
-  int speedReverse = 150;
-  
-  motorRotateConfig(ROTATE_RIGHT);
-  delay(100);
-  
-  for(timeToRotate = 0; timeToRotate < 100; timeToRotate++){
-    analogWrite(MOTOR_DRIVE_2, speedForward);
-    analogWrite(MOTOR_DRIVE_4, speedForward);
-    
-    analogWrite(MOTOR_DRIVE_1, speedReverse);
-    analogWrite(MOTOR_DRIVE_3, speedReverse);
-  delay(20);
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
+  {
+    rotateMotor(-240, 240); 
+  }
+  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
+  {
+    rotateMotor(240, -240); 
+  } 
+  else if (rightIRSensorValue == HIGH && leftIRSensorValue == HIGH && digitalRead(Backup_sensor) == LOW)
+  {
+    rotateMotor(MOTOR_SPEED, MOTOR_SPEED);
+  }
+  else
+  {
+    rotateMotor(0, 0);
   }
 }
 
-// Hàm thực thi việc quay bánh xe qua trái 
-// hàm phụ
-void motorRotateLeft(){
-  int timeToRotate;
-  int speedForward = 100;
-  int speedReverse = 150;
+
+void rotateMotor(int rightMotorSpeed, int leftMotorSpeed)    //ham di chuyen moto 
+{
   
-  motorRotateConfig(ROTATE_LEFT);
-  delay(100);
-  
-  for(timeToRotate = 0; timeToRotate < 100; timeToRotate++){
-    analogWrite(MOTOR_DRIVE_1, speedForward);
-    analogWrite(MOTOR_DRIVE_3, speedForward);
-    
-    analogWrite(MOTOR_DRIVE_2, speedReverse);
-    analogWrite(MOTOR_DRIVE_4, speedReverse);
-    delay(20);
+  if (rightMotorSpeed < 0)
+  {
+    digitalWrite(rightMotorPin1,LOW);
+    digitalWrite(rightMotorPin2,HIGH);    
   }
+  else if (rightMotorSpeed > 0)
+  {
+    digitalWrite(rightMotorPin1,HIGH);
+    digitalWrite(rightMotorPin2,LOW);      
+  }
+  else
+  {
+    delay(200);
+    if(rightMotorSpeed == 0)
+    { 
+      digitalWrite(rightMotorPin1,LOW);
+      digitalWrite(rightMotorPin2,LOW);
+    }      
+  }
+
+  if (leftMotorSpeed < 0)
+  {
+    digitalWrite(leftMotorPin1,LOW);
+    digitalWrite(leftMotorPin2,HIGH);    
+  }
+  else if (leftMotorSpeed > 0)
+  {
+    digitalWrite(leftMotorPin1,HIGH);
+    digitalWrite(leftMotorPin2,LOW);      
+  }
+  else 
+  {
+    digitalWrite(leftMotorPin1,LOW);
+    digitalWrite(leftMotorPin2,LOW);      
+  }
+  analogWrite(enableRightMotor, 200);
+  analogWrite(enableLeftMotor, 200);    
+}
+
+int sensorDetectionRange(int TRIG, int ECHO){  
+  sendSensorSignal(TRIG);
+  
+  int cm = receivedSensorSignal(ECHO);
+  
+  //Serial.print(cm);
+  //Serial.print("cm");
+  //Serial.println();
+  return cm;
+}
+
+// Hàm phát tín hiệu của cảm biến
+// nhận 1 params là số PORT phát/nhận tín hiệu
+void sendSensorSignal(int portNumber){
+  pinMode(portNumber, OUTPUT);
+  
+  digitalWrite(portNumber, LOW);
+  delayMicroseconds(2);
+  digitalWrite(portNumber, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(portNumber, LOW);
+}
+
+// Hàm nhận tín hiệu của cảm biến, trả về 1 con số theo cm ở kiểu long
+// nhận 1 params là số PORT phát/nhận tín hiệu
+long receivedSensorSignal(int portNumber){
+  long duration, cm;
+  pinMode(portNumber, INPUT);
+  
+  duration = pulseIn(portNumber, HIGH);
+  cm = microSecToCenti(duration);
+  
+  return cm;
+}
+
+// Hàm chuyển đổi tín hiệu nhận được của cảm biến thành cm, 
+// nhận 1 param là thời gian ở micro, 
+// trả về 1 con số theo cm ở kiểu long
+long microSecToCenti(long duration){
+  return duration / 29 / 2;
 }
